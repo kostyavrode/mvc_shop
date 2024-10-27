76d3e7f8a9e8a9eaeda05af7ad0f85f80a257d22
kostyavrode <inbloomf3@gmail.com>
2024-10-27 15:03:57 +0300

    last
    last

--- Assets/Scripts/Utils/TwoGameObjectArrays.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TwoGameObjectArrays
-{
-    public GameObject[] array1;
-    public GameObject[] array2;
-}
